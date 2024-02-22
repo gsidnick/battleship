7930fda1ffdc259aa930f740bddfb096a0f99fdd
Nikolay Sidorenko <gsidnick@gmail.com>
2024-02-22 15:32:11 +0300

    feat: implement bot
    (cherry picked from commit 1b657ea51549c68a242e210f9b86d4491dab0a69)

--- src/controllers/botController.ts
+++ src/controllers/botController.ts
@@ -1,8 +1,20 @@
 import db from '../db';
 import { botShips } from '../data/ships';
-import { GameStart, Map, PlayerShips, Response, Ships } from '../types';
+import { Coordinate, GameStart, Map, PlayerShips, Response, Ships, Winners } from '../types';
 import { stringifyResponse } from '../utils';
 import PlayerWebSocket from '../websocket';
+import {
+  checkWinner,
+  finishGame,
+  getAttackFeedback,
+  getAttackResult,
+  getOpponent,
+  getRandomShot,
+  getShipsFromGame,
+  moveTurn,
+} from './gameController';
+import { sendToAllClients, sendToSpecifyClients } from './clientController';
+import { setWinner, updateWinners } from './winnerController';
 
 const generateGameID = (): number => {
   const gameId = db.gameIndex + 1;
@@ -62,4 +74,30 @@ export const startGameWithBot = (ws: PlayerWebSocket, player: PlayerShips) => {
 
   const message: string = stringifyResponse(response);
   ws.send(message);
+};
+
+export const makeBotAttack = (ws: PlayerWebSocket, gameId: number) => {
+  const playersInGame = getShipsFromGame(gameId);
+  const opponent = getOpponent(0, playersInGame);
+  const playersId = [ws.player.index, opponent.indexPlayer];
+  const shot: Coordinate = getRandomShot(opponent.map);
+  const status = getAttackResult(shot, opponent);
+
+  if (status) {
+    const feedback = getAttackFeedback(status, shot, 0);
+    sendToSpecifyClients(feedback, playersId);
+  }
+
+  const isWinner = checkWinner(opponent.map);
+
+  if (isWinner) {
+    const winnerResponse = finishGame(0);
+    sendToSpecifyClients(winnerResponse, [ws.player.index]);
+    setWinner('bot');
+    const winners: Response<Winners> = updateWinners();
+    sendToAllClients(winners);
+  }
+
+  const turn = moveTurn(gameId, ws.player.index);
+  sendToSpecifyClients(turn, playersId);
 };
--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ import {
   checkWinner,
 } from './controllers/gameController';
 import { addClient, removeClient, sendToAllClients, sendToSpecifyClients } from './controllers/clientController';
-import { playGameWithBot, startGameWithBot } from './controllers/botController';
+import { makeBotAttack, playGameWithBot, startGameWithBot } from './controllers/botController';
 import { Response, PlayerResponse, Rooms, Winners, Coordinate } from './types.js';
 import { parseRequest, stringifyResponse } from './utils';
 import { isPlayerExist } from './db';
@@ -119,13 +119,40 @@ wss.on('connection', (ws: PlayerWebSocket) => {
       }
 
       case 'attack': {
+        const game = getGame(data.gameId);
+
         if (withBot) {
-          const turn = moveTurn(data.gameId, 0);
-          sendToSpecifyClients(turn, [ws.player.index]);
+          if (game.currentPlayer === ws.player.index) {
+            const shot: Coordinate = { x: data.x, y: data.y };
+            const playersInGame = getShipsFromGame(data.gameId);
+            const opponent = getOpponent(data.indexPlayer, playersInGame);
+            const status = getAttackResult(shot, opponent);
+
+            if (status) {
+              const feedback = getAttackFeedback(status, shot, ws.player.index);
+              sendToSpecifyClients(feedback, [ws.player.index]);
+            }
+
+            const isWinner = checkWinner(opponent.map);
+
+            if (isWinner) {
+              const winnerResponse = finishGame(ws.player.index);
+              sendToSpecifyClients(winnerResponse, [ws.player.index]);
+              setWinner(ws.player.name);
+              const winners: Response<Winners> = updateWinners();
+              sendToAllClients(winners);
+            }
+
+            const turn = moveTurn(data.gameId, 0);
+            sendToSpecifyClients(turn, [ws.player.index]);
+
+            const timeout = setTimeout(() => {
+              makeBotAttack(ws, data.gameId);
+              clearTimeout(timeout);
+            }, 3000);
+          }
           break;
         }
-
-        const game = getGame(data.gameId);
 
         if (game.currentPlayer === data.indexPlayer) {
           const shot: Coordinate = { x: data.x, y: data.y };
@@ -175,10 +202,19 @@ wss.on('connection', (ws: PlayerWebSocket) => {
           sendToSpecifyClients(winnerResponse, playersId);
           const winners: Response<Winners> = updateWinners();
           sendToAllClients(winners);
+          break;
         }
 
         const turn = moveTurn(data.gameId, opponent.indexPlayer);
         sendToSpecifyClients(turn, playersId);
+
+        if (withBot) {
+          const timeout = setTimeout(() => {
+            makeBotAttack(ws, data.gameId);
+            clearTimeout(timeout);
+          }, 3000);
+        }
+
         break;
       }
 
